54cb5693bc8f061a1a082b4fa4723a6482f74be2
preeti <118329523+lalpreeti@users.noreply.github.com>
2023-05-02 12:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+let firstName=document.querySelector('input]name=fname]').value;
+	let firstName=document.querySelector('input]name=lname]').value;
+	alert(firstName + " " + lastName);
 }
